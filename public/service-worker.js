453216c6e9c1f459f4d9323a522c83e8ec894d67
James Lovejoy <james.lovejoy2@gmail.com>
2020-03-03 18:15:44 -0600

    initial commit, created manifest and service-worker files, added service worker register to index.html

---
+++ public/service-worker.js
@@ -0,0 +1,66 @@
+const FILES_TO_CACHE = ["/", "/index.html", "app.js"];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install event for service worker
+self.addEventListener("install", function(evt) {
+    //This line is saying that work is ongoing until the promise settles, and it shouldn't terminate the service worker if it wants that work to complete.
+  evt.waitUntil(
+    //go to caches object and open file that matches CACHE_NAME variable.  Wait for that promise to resolve.
+    // when it does send console.log message and add all the files in Files_To_CAche variable to cache.
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      //Grab all the files in the Files_to_cache variable and add them to the cache object
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  //tells the newly installed service worker to skip the waiting state and move directly to activating state.
+  //6 states service worker  can be in parsed, installing, installed (waiting), activating, activated, redundant  
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    //returns a promise that resolves into an array of cache keys
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+        //goes through the array of cache keys and deletes them if they do not match the CACHE_name and DATA_Cache_NAME variables specified above
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+}});
